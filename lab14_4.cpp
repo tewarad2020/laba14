be19d8ac3643efa56565d579f2e057851ad0bf74
ford <tewarad2020>
2021-02-02 16:45:56 +0700

    code completed

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,52 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d) {
+	a = 50;
+	b = 100;
+	c = 500; 
+	d = 1000;
+	int h[] = {a,b,c,d};
+	int p;
+	
+	for (;;) {
+		p = rand()%4;
+		a = h[p];
+		if (a!=0) {
+			a=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+	
+	for (;;) {
+		p = rand()%4;
+		b = h[p];
+		if (b!=0) {
+			b=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+	
+	for (;;) {
+		p = rand()%4;
+		c = h[p];
+		if (c!=0) {
+			c=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+	
+	for (;;) {
+		p = rand()%4;
+		d = h[p];
+		if (d!=0) {
+			d=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+}
--- lab14_5.cpp
+++ lab14_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,58 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *w,int *x,int *y,int *z) {
+	*w = 50;
+	*x = 100;
+	*y = 500; 
+	*z = 1000;
+	int h[] = {*w,*x,*y,*z};
+	int p;
+	
+	for (;;) {
+		p = rand()%4;
+		*w = h[p];
+		if (*w!=0) {
+			*w=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+	
+	for (;;) {
+		p = rand()%4;
+		*x = h[p];
+		if (*x!=0) {
+			*x=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+	
+	for (;;) {
+		p = rand()%4;
+		*y = h[p];
+		if (*y!=0) {
+			*y=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+	
+	for (;;) {
+		p = rand()%4;
+		*z = h[p];
+		if (*z!=0) {
+			*z=h[p];
+	  		h[p] = 0;
+			break;		
+		}	
+	}
+}
